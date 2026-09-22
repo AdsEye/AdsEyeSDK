260b629dbfa1584f6643d70685249e4e2337b1a1
AdsEye SPM Publisher <spm-publisher@adseye.invalid>
2026-09-22 12:55:22 +0800

    release: AdsEyeAdSDK 1.4.21

--- Sources/AdsEyeResourceSupport/AdsEyeResourceSupport.m
+++ Sources/AdsEyeResourceSupport/AdsEyeResourceSupport.m
@@ -24,18 +24,6 @@ static NSBundle *AdsEyeSPMBundleForClass(id self, SEL command, Class targetClass
     return originalImplementation(self, command, targetClass);
 }
 
-static id AdsEyeSPMInitWithBundleName(id self, SEL command, NSString *bundleName) {
-    NSString *resourceName = bundleName.lastPathComponent.stringByDeletingPathExtension;
-    NSURL *bundleURL = [AdsEyeSPMResourceBundle URLForResource:resourceName
-                                                withExtension:@"bundle"];
-    if (!bundleURL) {
-        return nil;
-    }
-
-    // AdsEyeAdBundle/AdsEyeADXAdBundle 都是 NSBundle 子类，直接初始化目标资源 Bundle。
-    return [self initWithURL:bundleURL];
-}
-
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -49,18 +37,6 @@ static id AdsEyeSPMInitWithBundleName(id self, SEL command, NSString *bundleName
                             @selector(bundleForClass:),
                             (IMP)AdsEyeSPMBundleForClass,
                             method_getTypeEncoding(bundleForClassMethod));
-
-        // 两个资源类在不同 product 下按需存在，运行时只替换已经链接的类。
-        for (NSString *className in @[@"AdsEyeAdBundle", @"AdsEyeADXAdBundle"]) {
-            Class bundleClass = NSClassFromString(className);
-            Method method = class_getInstanceMethod(bundleClass, NSSelectorFromString(@"initWithBundleName:"));
-            if (bundleClass && method) {
-                class_replaceMethod(bundleClass,
-                                    NSSelectorFromString(@"initWithBundleName:"),
-                                    (IMP)AdsEyeSPMInitWithBundleName,
-                                    method_getTypeEncoding(method));
-            }
-        }
     });
 }
 
